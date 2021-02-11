8283c574fa21c581f42c157a6581e87b19cf270c
gluegun88 <gluegun88@users.noreply.github.com>
2021-02-11 12:23:53 -0500

    Pet instance variables, constructor complete and get methods complete

--- src/Pet.java
+++ src/Pet.java
@@ -5,4 +5,31 @@
 
 public class Pet {
 
+    private String name;
+    private String type;
+    private int age;
+    private String colour;
+
+    public Pet(String name, String type, int age, String colour) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColour() {
+        return colour;
+    }
 }
